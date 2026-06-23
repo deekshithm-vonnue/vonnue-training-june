0f7111e99ca92828b3e45274811f6d5cbfc1b6e8
deekshithm-vonnue <deekshith.m@vonnue.com>
2026-06-23 17:28:09 +0530

    feat: complete Task 1 - Bubbling, Capturing & stopPropagation

---
+++ week3/day-2/task-1/script.js
@@ -0,0 +1,42 @@
+const topObj = document.getElementById("top-container");
+const middle = document.getElementById("middle-container");
+const bottom = document.getElementById("bottom-container");
+
+topObj.addEventListener("click", (e) => {
+  e.stopPropagation();
+  console.log("top-conatiner catched, " + "source element: " + e.srcElement.id);
+});
+
+middle.addEventListener("click", (e) => {
+  e.stopPropagation();
+  console.log(
+    "middle-conatiner catched, " + "source element: " + e.srcElement.id,
+  );
+});
+
+bottom.addEventListener("click", (e) => {
+  e.stopPropagation();
+  console.log(
+    "bottom-conatiner catched, " + "source element: " + e.srcElement.id,
+  );
+  e.stopImmediatePropagation();
+});
+
+bottom.addEventListener("click", (e) => {
+  e.stopPropagation();
+  console.log(
+    "2nd time - bottom-conatiner catched, " +
+      "source element: " +
+      e.srcElement.id,
+  );
+});
+
+const buttonObj = document.getElementById("submit-button");
+buttonObj.addEventListener("click", (e) => {
+  e.preventDefault();
+});
+
+const anchorObj = document.getElementById("anchor-tag");
+anchorObj.addEventListener("click", (e) => {
+  e.preventDefault();
+});
